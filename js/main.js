057b4153b2cbe203badc47aab3f36e695ebf58c9
Marieta28 <69346003+Marieta28@users.noreply.github.com>
2021-03-20 20:51:11 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop:true,
   navigation:{
     nextEl: '.arrow',
+  },
+  breakpoints: {
+
+    540: {
+      slidesPerView: 2,
+    }
   }
 });
+var menubutton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header')
+menubutton.addEventListener('click', function() {
+  menubutton.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
+})
